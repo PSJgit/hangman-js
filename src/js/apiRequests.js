e89e83b1ef55422ca740f925b4aba94d9f71bedd
PSJBit <patricksebastiandesign@gmail.com>
2019-02-28 20:30:03 +1000

    api update, score dom refactor

--- src/js/apiRequests.js
+++ src/js/apiRequests.js
@@ -7,30 +7,35 @@
 // !!!!!!! But it will still work
 
 let key
-
-try {
-	key = hangmanConfig.KEY
-} catch(err) {
-	console.warn('No Api Key provided - game will still run but with less words to choose from', err )
-}
+let api
 
 const min = 5
 const max = 10
 
-// excluded some types of speech and set word commonality to make the game more fun
-const randomWord = `https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=noun%2C%20adjective%2C%20verb%2C%20adverb&minCorpusCount=100000&maxCorpusCount=-1&minDictionaryCount=10&maxDictionaryCount=-1&minLength=${min}&maxLength=${max}&api_key=${key}`
+try {
+	key = hangmanConfig.KEY
+	// excluded some types of speech and set word commonality to make the game more fun
+	api = `https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech=noun%2C%20adjective%2C%20verb%2C%20adverb&minCorpusCount=100000&maxCorpusCount=-1&minDictionaryCount=10&maxDictionaryCount=-1&minLength=${min}&maxLength=${max}&api_key=${key}`
+} catch(err) {
+	console.warn('No Api Key provided - the game will still run, but with less words to choose from', err )
+}
 
 const getRandomWord = async () => {
 	
-	try {
-		const response = await fetch(randomWord)
+	if (api) {
+		try {
+			const response = await fetch(api)
 			if (response.status === 200) {
-			const data = await response.json()
-			return data.word
-		} 
-	} catch (err) {
-		//console.warn(err)
+				const data = await response.json()
+				return data.word
+			} 
+		} catch (err) {
+			//console.warn(err)
+		}
+	} else {
+		return undefined
 	}
+	
 }
 
 export default getRandomWord
